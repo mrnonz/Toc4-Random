0add2b5e9645f302dabd1d2286dd1a736a0c8d99
Nontawat Numor <nontawat39@gmail.com>
2016-10-10 20:52:27 +0700

    test function

--- 2Lib.php
+++ 2Lib.php
@@ -1,7 +1,19 @@
 <?php
-    $Number = pow(10,5);
+    $Number = pow(10,2);
     $Randomtime = pow(10,2);
     $data = array_fill(0, $Number, 0);
 
-    echo rand(0, $Number);
+    //echo rand(0, $Number);
+
+    for ($i=0; $i < $Number; $i++) {
+        $data[rand(0, $Number)]++;
+    }
+    //var_dump($data);
+    
+    $myfile = fopen("data-2.txt", "w") or die("Unable to open file!");
+    $txt = "John Doe\n";
+    fwrite($myfile, $txt);
+    $txt = "Jane Doe\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
  ?>
