93e3b7b67d536ba940e4ff4a2c23eee514092ba3
Nontawat Numor <nontawat39@gmail.com>
2016-10-10 22:01:09 +0700

    save data 3

--- 3Lib.pl
+++ 3Lib.pl
@@ -1,15 +1,25 @@
-$Number = 10**2;
-$Randomtime =  10**2;
-@data = (0) x 5;
+use strict;
+use warnings;
+
+my $Number = 10**5;
+my $Randomtime =  10**8;
+my @data = (0) x 5;
 
 for (my $i = 0; $i < $Randomtime; $i++) {
     $data[int(rand($Number))]++;
 }
 
+open(my $fh, '>', 'data-3.txt');
 for (my $i = 0; $i < $Number; $i++) {
     if (!$data[$i]) {
         $data[$i] = 0;
     }
 
+    my $str = $i." ".$data[$i]."\n";
+
+    #print($str);
+    print $fh $str;
     #print($i." ".$data[$i]."\n");
 }
+
+close $fh;
