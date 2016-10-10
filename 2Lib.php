b822dc07bbc3e6603c96b3ee060758ef56423695
Nontawat Numor <nontawat39@gmail.com>
2016-10-10 21:09:01 +0700

    save data 2

--- 2Lib.php
+++ 2Lib.php
@@ -1,19 +1,22 @@
 <?php
-    $Number = pow(10,2);
-    $Randomtime = pow(10,2);
+    $Number = pow(10,5);
+    $Randomtime = pow(10,8);
     $data = array_fill(0, $Number, 0);
 
     //echo rand(0, $Number);
 
-    for ($i=0; $i < $Number; $i++) {
-        $data[rand(0, $Number)]++;
+    for ($i=0; $i < $Randomtime; $i++) {
+        $data[rand(0, $Number-1)]++;
     }
     //var_dump($data);
-    
-    $myfile = fopen("data-2.txt", "w") or die("Unable to open file!");
-    $txt = "John Doe\n";
-    fwrite($myfile, $txt);
-    $txt = "Jane Doe\n";
-    fwrite($myfile, $txt);
+
+    $myfile = fopen("data-2.txt", "wb") or die("Unable to open file!");
+
+    for ($i=0; $i < $Number; $i++) {
+        $txt = $i.' '.$data[$i]."\n";
+        //echo $txt;
+        fwrite($myfile, $txt);
+    }
+
     fclose($myfile);
  ?>
