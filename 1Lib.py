e0b0e04c4f166b98737143f8693e11ccbf3fa368
Nontawat Numor <nontawat39@gmail.com>
2016-10-10 20:23:09 +0700

    save data 1

--- 1Lib.py
+++ 1Lib.py
@@ -1,9 +1,19 @@
 import random
 
-Number = 10
+Number = 10**5
+Randomtime = 10**8
 data = [0] * Number
 
-for i in range(Number):
+for i in range(Randomtime):
     data[random.randrange(Number)] += 1
 
-print data
+# print data
+
+fo = open("data-1.txt", "wb")
+print "Name of the file: ", fo.name
+
+for i in range(Number):
+    dataOut = str(i) + ' ' + str(data[i]) + '\n'
+    fo.write(dataOut)
+
+fo.close()
